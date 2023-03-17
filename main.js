be9388d1d77e224e05dee5893bc24121363cc5d1
ajmris <szymektarasiewicz@onet.pl>
2023-03-17 23:49:52 +0100

    Animacja mandagory

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var ctx = canvas.getContext('2d');
 var c = canvas.getContext('2d');
 var image = new Image();
 image.src = 'graphics/bg.png';
+
 // Canvas full screan
 canvas.style.cssText='width: 100%; height: 100%;';
 // Draw the initial background image
@@ -48,3 +49,62 @@ setInterval(function(){
 },100)
 
 draw();
+
+/*var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+var c = canvas.getContext('2d');
+var image = new Image();
+image.src = 'graphics/bg.png';
+
+// Canvas full screan
+canvas.style.cssText='width: 100%; height: 100%;';
+// Draw the initial background image
+ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+var bgPosX = 0;
+function update() {
+  // Move the background image to the left
+  bgPosX -= 1;
+  // If the background image goes offscreen to the left, reset its position to the right
+  if (bgPosX < -canvas.width) {
+    bgPosX = 0;
+  }
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // Draw the two background images
+  ctx.drawImage(image, bgPosX, 0, canvas.width, canvas.height);
+  ctx.drawImage(image, bgPosX + canvas.width, 0, canvas.width, canvas.height);
+  // Draw the character
+  c.drawImage(images[i],100, 740,200,200);
+}
+function draw() {
+  update();
+  requestAnimationFrame(draw);
+}
+
+//character animation
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+//create empty array
+var images=[];
+images.length=10;
+//push the image to array
+for(var i=1; i<images.length; i++){
+  images[i]=new Image();
+  images[i].src='graphics/Walk ('+i.toString()+').png';
+}
+var i=1;
+setInterval(function(){
+  i++;
+  if(i>=10){
+    i=1;
+  }
+},100)
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'j') {
+    // Move the images up by 200px using the GSAP library
+    gsap.to(images, {duration: 1, y: -200});
+  }
+});
+
+draw();*/
